0b27c0ec25c4529747fe81cdb6a29f0978592816
adil-micromerger <adishinwari69@gmail.com>
2024-02-20 17:02:10 +0500

    module 8 completed

--- models/product.js
+++ models/product.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 const products = [];
- const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
-  );
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 const getProductsFromFile = (cb) => {
- 
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -19,8 +18,11 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
   save() {
     getProductsFromFile((products) => {
--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,10 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/cart',shopController.getCart);
+router.get('/cart', shopController.getCart);
 
-router.get('/checkout',shopController.getCheckout);
+router.get('/checkout', shopController.getCheckout);
+
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
