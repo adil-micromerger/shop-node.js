fd9fb8343a3a54fe8e7a43aec614cfcdec805bf4
adil-micromerger <adishinwari69@gmail.com>
2024-02-20 19:31:07 +0500

    add to cart

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,13 @@ const express = require('express');
 const router = express.Router();
 router.get('/', shopController.getIndex);
 
+router.get('/products/:productId',shopController.getProduct);
+
 router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart',shopController.postCart)
 
 router.get('/checkout', shopController.getCheckout);
 
